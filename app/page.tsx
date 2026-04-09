266b847a8fca70882f6851419d6342b9beabed02
Agrima Bhutani <agrimab@Agrimas-MBP.attlocal.net>
2026-04-09 08:07:13 -0700

    Tighten landing hero line break

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,11 @@ export default function Home() {
           fontWeight: 400,
         }}
       >
-        are you ready to <em style={{ color: "#1D9E75" }}>ace it</em>?
+        <>
+          are you ready to
+          <br />
+          <em style={{ color: "#1D9E75" }}>ace it</em>?
+        </>
       </h1>
 
       <p
